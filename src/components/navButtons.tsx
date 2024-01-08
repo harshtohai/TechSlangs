c0199b262cb86f1b594a9489e40a628020b47d97
Harsh Panchal <harsh.panchal.me@gmail.com>
2024-01-08 14:04:22 +0530

    UPDATE: Dynamic routing

--- src/components/navButtons.tsx
+++ src/components/navButtons.tsx
@@ -1,19 +1,22 @@
 'use client'
+import Link from 'next/link'
+
 
 export default function NavButtons() {
     return(
         <div className="w-full flex justify-between content-center flex-wrap">
-            <a href="/new-word">            
+            <Link href={'/new-word'}>    
                 <button className="h-[3rem] w-[19.7rem] my-[0.2rem] mb-[0.8rem] bg-pribg flex items-center justify-center rounded-[4px] hover:border-[1px] border-0 border-text ">
                     <p>Add New Word</p>
                 </button>
-            </a>
-                
-            <a href="/profile">
+            </Link>
+
+            <Link href={'/profile'}>    
                 <button className="h-[3rem] w-[19.7rem] my-[0.2rem] mb-[0.8rem] bg-pribg flex items-center justify-center rounded-[4px] hover:border-[1px] border-0 border-text ">
-                    <p>Your Profile</p>
+                    <p>Login</p>
                 </button>
-            </a>
+            </Link>
+
         </div>
 )
 }
