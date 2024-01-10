798112f5771b604d1582ba84213b3f543cf3094e
Harsh Panchal <harsh.panchal.me@gmail.com>
2024-01-10 15:23:30 +0530

    Creation of Data schema and ENV variable

--- src/components/navButtons.tsx
+++ src/components/navButtons.tsx
@@ -1,12 +1,13 @@
 'use client'
+
 import Link from 'next/link'
 
 
 export default function NavButtons() {
     return(
         <div className="w-full flex justify-between content-center flex-wrap">
-            <Link href={'/new-word'}>    
-                <button className="h-[3rem] w-[19.7rem] my-[0.2rem] mb-[0.8rem] bg-pribg flex items-center justify-center rounded-[4px] hover:border-[1px] border-0 border-text ">
+            <Link href={'/new-word'} className='w-full'>    
+                <button className="h-[3rem] w-full my-[0.2rem] mb-[0.8rem] bg-pribg flex items-center justify-center rounded-[4px] hover:border-[1px] border-0 border-text ">
                     <p>Add New Word</p>
                 </button>
             </Link>
