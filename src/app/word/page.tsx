2510fa045feb47f254c069d72a88569df482bc1a
Harsh Panchal <harsh.panchal.me@gmail.com>
2024-01-13 01:41:42 +0530

    Worked on word page.

--- src/app/word/page.tsx
+++ src/app/word/page.tsx
@@ -1,8 +1,14 @@
+import Post from "@/components/post";
+
 export default function WordPage() {
     return(
         <main className="flex h-full w-full justify-center bg-secbg">
-            <div className="w-[47%] h-full flex flex-col justify-center items-center">
-                
+            <div className="w-[47%] h-full flex flex-col bg-pribg items-center my-[1rem] rounded-[4px]">
+                <Post word='Cooked' by="Anon" description="Processed. Said of data that has been manipulated in some manner. Contrast with raw. See baked." upcount="69" downcount="37" commentscount="24" />
+                <div className="h-[2px] w-full px-[2rem]">
+                    <div className="h-full w-full bg-text"></div>
+                </div>
+
             </div>
         </main>  
     )
