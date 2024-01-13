9ada2f3a0dae926c2f4d6a3a82cd725aea5609ea
Harsh Panchal <harsh.panchal.me@gmail.com>
2024-01-13 14:15:19 +0530

    styling changes

--- src/app/word/page.tsx
+++ src/app/word/page.tsx
@@ -1,14 +1,15 @@
+import Comment from "@/components/comment";
 import Post from "@/components/post";
 
 export default function WordPage() {
     return(
-        <main className="flex h-full w-full justify-center bg-secbg">
-            <div className="w-[47%] h-full flex flex-col bg-pribg items-center my-[1rem] rounded-[4px]">
+        <main className="flex w-full justify-center bg-secbg">
+            <div className="w-[47%] flex flex-col bg-pribg items-center my-[1rem] rounded-[4px]">
                 <Post word='Cooked' by="Anon" description="Processed. Said of data that has been manipulated in some manner. Contrast with raw. See baked." upcount="69" downcount="37" commentscount="24" />
-                <div className="h-[2px] w-full px-[2rem]">
-                    <div className="h-full w-full bg-text"></div>
-                </div>
-
+                <Comment description="Processed. Said of data that has been manipulated in some manner. Contrast with raw. See baked." upvotes="69" downvotes="37" id="1"/>
+                <Comment description="Processed. Said of data that has been manipulated in some manner. Contrast with raw. See baked." upvotes="69" downvotes="37" id="2"/>
+                <Comment description="Processed. Said of data that has been manipulated in some manner. Contrast with raw. See baked." upvotes="69" downvotes="37" id="3"/>
+                <Comment description="Processed. Said of data that has been manipulated in some manner. Contrast with raw. See baked." upvotes="69" downvotes="37" id="4"/>
             </div>
         </main>  
     )
