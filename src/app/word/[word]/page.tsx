99b379fd8d9a7a9d02adb1ff591cd2a9a899abea
Harsh Panchal <harsh.panchal.me@gmail.com>
2024-04-01 20:14:51 +0530

    abother attempt

--- src/app/word/[word]/page.tsx
+++ src/app/word/[word]/page.tsx
@@ -5,17 +5,17 @@ import Link from "next/link";
 import NavButtons from "@/components/navButtons";
 
 
-export default async function WordSearch({word}:{word:string}){
+export default async function WordSearch({params}:{params:any}){
     const session = await auth()
     async function Posts(){
-        const words = (await findWord(word)).Posts
+        const words = (await findWord(params.word)).Posts
         if (words.length > 0 ){
             return words.map((word)=>{
                     return <Post word={word.word} by={word.User?.name ? word.User.name : 'user'} description={word.description} upcount={word.upvotes} downcount={word.downvotes} key={word.id} commentscount={word.commentscount} sessionUserId={session?.user?.id} userId={word.userId}  id={word.id} />
             })
         }
         else{
-            return <div className="w-full h-full justify-center items-center"><h1 className="text-text">No matching word was found. You can add <Link href='/new-word'>{word}</Link></h1></div>
+            return <div className="w-full h-full justify-center items-center"><h1 className="text-text">No matching word was found. You can add <Link href='/new-word'>{params.word}</Link></h1></div>
         }
     }
     
