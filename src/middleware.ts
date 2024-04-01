421932e4d80b6a8957df7c74d25f576f5210f5a9
Harsh Panchal <harsh.panchal.me@gmail.com>
2024-04-01 20:20:27 +0530

    fixes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,9 @@ import NextAuth from "next-auth"
 import { NextResponse } from "next/server";
 import authConfig from '../auth.config'
 
-const {auth} = NextAuth(authConfig)
+const {auth}:any = NextAuth(authConfig)
 
-export default auth((req) => {
+export default auth((req:any) => {
   
   const isLoggedIn = !!req.auth;
   const isAuthPage = req.nextUrl.pathname.startsWith("/auth")
